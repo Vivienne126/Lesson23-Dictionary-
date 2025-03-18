5561ed051ec54cced4e25365a06cb31795145dcb
Vivienne126 <vivienne.singh@cns.ac.in>
2025-03-18 22:07:46 +0530

    New2

---
+++ Activity3.py
@@ -0,0 +1,6 @@
+Country_code={"India" : "0091" , "Australia" : "0025" , "Nepal" : "0096" }
+print("The country Code for India is:")
+print(Country_code.get("India" , "Not Found"))
+print("The country Code for Japan is:")
+print(Country_code.get("Japan" , "Not Found"))
+
